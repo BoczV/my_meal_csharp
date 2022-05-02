ae55e5346e96e33588b2e89af19fa57a4824b379
Viktor <beviktor95@gmail.com>
2022-05-02 17:19:13 +0200

    Template of DBConnectionDao class is done

---
+++ ConsoleApp1/Repository/Implementation/DBConnectionDao.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace ConsoleApp1.Repository.Implementation;
+
+public class DBConnectionDao: Dao<string>
+{
+    public string get(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<string> getAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string getRandom()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void add(string t)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string update(string t)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string delete(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string deleteAll()
+    {
+        throw new System.NotImplementedException();
+    }
+}
